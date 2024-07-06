1d31da8db961100e2f0aa4a3c2a30c78d4d18749
Chris <117781333+chrism-la@users.noreply.github.com>
2024-07-06 12:57:33 -0700

    Created function to change the aria-expanded state on Open button to true with click event listener

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const btnOpen = document.querySelector('#btnOpen');
+
+function openMobileMenu() {
+    btnOpen.setAttribute('aria-expanded', 'true');
+}
+
+btnOpen.addEventListener('click', openMobileMenu);
