e7ab5104f91a63971394559fd4aa95dc5d4dde6e
Chris <117781333+chrism-la@users.noreply.github.com>
2024-07-06 13:18:15 -0700

    Created functionality for closeMobileMenu in script file Created function to change the aria-expanded state on Open button to flase with click event listener on close button

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
 
 function openMobileMenu() {
     btnOpen.setAttribute('aria-expanded', 'true');
 }
 
+function closeMobileMenu() {
+    btnOpen.setAttribute('aria-expanded', 'false');
+}
+
 btnOpen.addEventListener('click', openMobileMenu);
+btnClose.addEventListener('click', closeMobileMenu);
