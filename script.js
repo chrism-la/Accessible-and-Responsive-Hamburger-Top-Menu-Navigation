fe67eef3321a146c1e8629efcdb7a8db01b6cc35
Chris <117781333+chrism-la@users.noreply.github.com>
2024-07-06 13:22:32 -0700

    Used window matchMedia method to target 40em viewport width 'mobile viewport' and saved it as a constant

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btnOpen = document.querySelector('#btnOpen');
 const btnClose = document.querySelector('#btnClose');
+const media = window.matchMedia('(width < 40em)');
 
 function openMobileMenu() {
     btnOpen.setAttribute('aria-expanded', 'true');
