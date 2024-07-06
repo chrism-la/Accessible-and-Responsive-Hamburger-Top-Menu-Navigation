5f0f090d58020a617f6e57e05e15f56ebaffb0ac
Chris <117781333+chrism-la@users.noreply.github.com>
2024-07-06 14:14:26 -0700

    Locked scrolling when menu is open and unlocked it when closed menu

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const btnClose = document.querySelector('#btnClose');
 const media = window.matchMedia('(width < 40em)');
 const topNavMenu = document.querySelector('.topnav__menu');
 const main = document.querySelector('main');
+const body = document.querySelector('body');
 
 function setupTopNav(e) {
     if (e.matches) {
@@ -22,12 +23,14 @@ function openMobileMenu() {
     topNavMenu.removeAttribute('inert');
     topNavMenu.removeAttribute('style');
     main.setAttribute('inert', '');
+    bodyScrollLockUpgrade.disabledBodyScroll(body);
 }
 
 function closeMobileMenu() {
     btnOpen.setAttribute('aria-expanded', 'false');
     topNavMenu.setAttribute('inert', '');
     main.removeAttribute('inert');
+    bodyScrollLockUpgrade.enableBodyScroll(body);
 
     setTimeout(() => {
         topNavMenu.style.transition = 'none';
